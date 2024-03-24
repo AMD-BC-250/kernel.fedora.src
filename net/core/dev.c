ddb16e014d9a6d8885b62f3722582b329d296326
Clark Williams <williams@redhat.com>
2024-03-24 00:07:32 -0500

    net/core/dev.c: remove duplicate def of skb_defer_free_flush()
    Signed-off-by: Clark Williams <williams@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -6779,27 +6779,6 @@ static int napi_thread_wait(struct napi_struct *napi)
 	return -1;
 }
 
-static void skb_defer_free_flush(struct softnet_data *sd)
-{
-	struct sk_buff *skb, *next;
-
-	/* Paired with WRITE_ONCE() in skb_attempt_defer_free() */
-	if (!READ_ONCE(sd->defer_list))
-		return;
-
-	spin_lock(&sd->defer_lock);
-	skb = sd->defer_list;
-	sd->defer_list = NULL;
-	sd->defer_count = 0;
-	spin_unlock(&sd->defer_lock);
-
-	while (skb != NULL) {
-		next = skb->next;
-		napi_consume_skb(skb, 1);
-		skb = next;
-	}
-}
-
 static void napi_threaded_poll_loop(struct napi_struct *napi)
 {
 	struct softnet_data *sd;
